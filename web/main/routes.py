b23eef778c3e815a7ea7c81050440b1a6be2adbf
jiwon-07 <gigisong07@gmail.com>
2026-04-30 04:42:26 +0900

    update profile page

--- web/main/routes.py
+++ web/main/routes.py
@@ -1,30 +1,35 @@
-from flask import render_template, request
+from flask import render_template, request, session
 from . import main
 from web.auth.utils import require_login
 
-# Home page
 @main.route('/')
 @main.route('/landing_page')
 def landing_page():
     return render_template('main/landing_page.html')
 
-# About page
 @main.route('/about')
 def about():
     return render_template('main/about.html')
 
-# Dashboard
 @main.route('/dashboard')
 @require_login
 def dashboard():
-    return render_template('main/dashboard.html')  
+    return render_template('main/dashboard.html')
 
-# Error page
+@main.route('/profile')
+@require_login
+def profile():
+    return render_template('main/profile.html')
+
+@main.route('/edit_profile')
+@require_login
+def edit_profile():
+    return render_template('main/edit_profile.html')
+
 @main.app_errorhandler(404)
 def page_not_found(e):
     return f"{request.path} not exist", 404
 
 @main.route('/check-session')
 def check_session():
-    from flask import session
     return {"user": session.get("user")}
