0024c4d2283023bccdb261a439fbb8f783b6ac78
Tom Tran <quangthong11@gmail.com>
2026-05-01 01:05:41 +0800

    Organised: Move drop down JS to a seperate file for readability

--- web/static/js/base.js
+++ web/static/js/base.js
@@ -3,11 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     alerts.forEach(alert => {
         setTimeout(() => {
-            alert.classList.add("flash-hide"); // fade out
+            alert.classList.add("flash-hide");
 
             setTimeout(() => {
-                alert.remove(); // remove after fade
+                alert.remove();
             }, 500);
-        }, 2000); // show for 2 seconds
+        }, 2000);
+    });
+
+    //Profile dropdown logic
+    const profileBtn = document.querySelector(".profile-icon-btn");
+    const dropdown = document.getElementById("profileDropdown");
+    const menu = document.querySelector(".profile-menu");
+
+    // Toggle dropdown
+    if (profileBtn && dropdown) {
+        profileBtn.addEventListener("click", (e) => {
+            e.stopPropagation(); // prevent instant close
+            dropdown.classList.toggle("open");
+        });
+    }
+
+    //Close when clicking outside
+    document.addEventListener("click", (e) => {
+        if (menu && dropdown && !menu.contains(e.target)) {
+            dropdown.classList.remove("open");
+        }
     });
 });
