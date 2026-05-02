99325c153e2d6cfd8470e8658c4f34f0f7ccad9e
Ben Gilmore <23706738@student.uwa.edu.au>
2026-05-02 13:00:33 +0800

    New helper function that is the core of calculating user compatability matches

--- web/matching/service.py
+++ web/matching/service.py
@@ -40,3 +40,53 @@ def get_all_other_users(username):
 
     #Finally we return the list of usernames that have taken a quiz that can potentially be a match
     return usernames
+
+ #This function compares two users answers and then calculated their percentage match
+def calculate_match_percentage(current_user_answers, other_user_answers):
+    #This is a list that will store users that both users have answered
+    overlapping_questions = []
+
+    #We loop through every question answered by the current user
+    for question_key in current_user_answers:
+        #If that question has also been done by the other user
+        if question_key in other_user_answers:
+            #We add that question into our list 
+            overlapping_questions.append(question_key)
+
+    #If the users have both not done the same quiz, we return none as not match can be calculated
+    if not overlapping_questions:
+        return None, []
+
+    #This variable stores the current total of the two users compatability
+    total_compatibility = 0
+    #This creates a list to store the name of quizzes that both users have completed
+    shared_quizzes = []
+
+    #For each question that both users answered, we get the current and other users score for the same question
+    for question_key in overlapping_questions:
+        current_score = current_user_answers[question_key]
+        other_score = other_user_answers[question_key]
+
+        #We then calculate the absolute difference between the two answered questions to calculate the differences in their scores
+        difference = abs(current_score - other_score)
+        #We then convert that difference value into a percentage
+        #If both users answered 5 to the same question, their abs difference is 0, 1 - (0 / 4) = 1 which means their compatibility for the question is 100%
+        #We divide the difference by 4 because 4 is the largest difference two users can have for the same question
+        question_compatibility = 1 - (difference / 4)
+
+        #We then add the questions compatibility score to the total compatability variable for the users
+        total_compatibility += question_compatibility
+
+        #We then split the unique key pair question_key back into its seperate fields
+        quiz_name, question_index = question_key
+
+        #We check if the quiz_name has already been recorded as a shared quiz between the two users
+        if quiz_name not in shared_quizzes:
+            #If not we append the shared quiz into out list
+            shared_quizzes.append(quiz_name)
+
+    #Finally, we use our total compatability variable and convert it into a percentage
+    match_percentage = (total_compatibility / len(overlapping_questions)) * 100
+
+    #We then return the final matching percentage rounded to 2 decimal places alongside the list of shared quizzes both users did
+    return round(match_percentage, 2), shared_quizzes
