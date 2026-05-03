213e7ce2364316f0673a9c3da6f477efde085205
Ben Gilmore <23706738@student.uwa.edu.au>
2026-05-03 14:26:11 +0800

    UPDATE: Added in the function to use the fake users data to actually make the users and adding the changes to the DB to be saved.

--- populating_DB.py
+++ populating_DB.py
@@ -66,3 +66,34 @@ populated_user_answers = {
         "social_energy": [3, 4, 3, 3, 3, 3, 2, 3, 2, 3]
     }
 }
+
+#This function then actually creates the users using our fake data
+def create_populate_users():
+    #We loop through each fake user and check if their username already exists in the DB
+    for user_data in populate_users:
+        existing_user = User.query.filter_by(username=user_data["username"]).first()
+
+        #If the user already exists, we simply update their details rather then delete
+        if existing_user:
+            existing_user.displayname = user_data["displayname"]
+            existing_user.email = user_data["email"]
+            existing_user.password = generate_password_hash(user_data["password"])
+            existing_user.languages = user_data["languages"]
+            existing_user.units = user_data["units"]
+            existing_user.reset_token_version = 0
+
+        #If the user does not exist, we just add the user to the DB alongisde their fields
+        else:
+            user = User(
+                username=user_data["username"],
+                displayname=user_data["displayname"],
+                email=user_data["email"],
+                password=generate_password_hash(user_data["password"]),
+                languages=user_data["languages"],
+                units=user_data["units"],
+                reset_token_version=0
+            )
+            #We then add the user to the DB
+            db.session.add(user)
+    #And here we save all the user changes in the DB
+    db.session.commit()
