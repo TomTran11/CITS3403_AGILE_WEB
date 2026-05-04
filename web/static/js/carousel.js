336f316c12497b58d89cbf758e65214050d3d006
princie <paperprincie@gmail.com>
2026-05-04 16:19:45 +0800

    allowed to cycle through items

--- web/static/js/carousel.js
+++ web/static/js/carousel.js
@@ -2,21 +2,53 @@
 document.querySelectorAll(".carousel").forEach(carousel => {
     const items = carousel.querySelectorAll(".carousel-item");
 
-    const buttons = carousel.querySelectorAll(".carousel-item");
-
     const prev = carousel.querySelectorAll(".carousel-control-prev");
 
     const next = carousel.querySelectorAll(".carousel-control-next");
 
+    let current = 0;
+
+    let ready = true;
+
+    function hide(direction){
+        ready = false
+        $(items[current]).addClass(direction); //adds left or right to classes
+        $(items[current]).removeClass("active"); //removes active from classes
+        console.log(items[current].className);
+    }
+
+        function show(direction){
+        ready = false
+        $(items[current]).addClass(direction); //adds left or right to classes
+        $(items[current]).addClass("active"); //adds active to classes
+        console.log(items[current].className);
+    }
+
     $(prev).on('click', function(){
         console.log("previous" + carousel.id)
-    })
+        if (current != 0){
+            hide('left')
+            current -= 1;
+            show('right')
+        }
+        
+    });
 
     $(next).on('click', function(){
         console.log("next" + carousel.id)
+        if (current != items.length - 1){
+            hide('right')
+            current += 1;
+            show('left')
+        }
+    });
+
+    items.forEach(item => {
+        console.log($(item).offset())
     })
 
-    
 
 
 });
+
+
