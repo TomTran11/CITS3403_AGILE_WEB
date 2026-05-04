35294d5a8da3388e4819886bf34815499f8bb148
princie <paperprincie@gmail.com>
2026-05-04 16:21:50 +0800

    allow to loop through carousels

--- web/static/js/carousel.js
+++ web/static/js/carousel.js
@@ -6,45 +6,61 @@ document.querySelectorAll(".carousel").forEach(carousel => {
 
     const next = carousel.querySelectorAll(".carousel-control-next");
 
+    let loop = true;
+
     let current = 0;
 
     let ready = true;
 
     function hide(direction){
-        ready = false
+        ready = false;
         $(items[current]).addClass(direction); //adds left or right to classes
         $(items[current]).removeClass("active"); //removes active from classes
         console.log(items[current].className);
     }
 
         function show(direction){
-        ready = false
+        ready = false;
         $(items[current]).addClass(direction); //adds left or right to classes
         $(items[current]).addClass("active"); //adds active to classes
         console.log(items[current].className);
     }
 
     $(prev).on('click', function(){
-        console.log("previous" + carousel.id)
+        console.log("previous" + carousel.id);
         if (current != 0){
-            hide('left')
+            hide('left');
             current -= 1;
-            show('right')
+            show('right');
+        }
+        else{
+            if (loop == true){
+                hide('left');
+                current = items.length - 1;;
+                show('right');
+            }
         }
         
     });
 
     $(next).on('click', function(){
-        console.log("next" + carousel.id)
+        console.log("next" + carousel.id);
         if (current != items.length - 1){
-            hide('right')
+            hide('right');
             current += 1;
-            show('left')
+            show('left');
+        }
+        else{
+            if (loop == true){
+                hide('left');
+                current = 0;
+                show('right');
+            }
         }
     });
 
     items.forEach(item => {
-        console.log($(item).offset())
+        console.log($(item).offset());
     })
 
 
