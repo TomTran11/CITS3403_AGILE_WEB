9436bf1916705998c473419ef5e8188fad96d810
princie <paperprincie@gmail.com>
2026-05-08 00:21:03 +0800

    Added comments and removed unneccesary lines

--- web/static/js/carousel.js
+++ web/static/js/carousel.js
@@ -1,69 +1,75 @@
 
 document.querySelectorAll(".carousel").forEach(carousel => {
+
+    // CONSTANT DEFINITIONS //
     const items = carousel.querySelectorAll(".carousel-item");
-
     const prev = carousel.querySelectorAll(".carousel-control-prev-icon");
-
     const next = carousel.querySelectorAll(".carousel-control-next-icon");
 
-    let loop = true;
-
+    // VARIABLE DEFINITIONS //
     let current = 0;
-
     let upcoming = 0;
-
     let ready = true;
-
+    let loop = true;
+    
+    /*
+    * this function is used to hide a carousel item to either the left or right
+    * side of the screen. Takes one parameter, string direction, which is either
+    * 'left' or 'right'.
+    */
     function hide(direction){
+        //get classes from direction
         const direction_class = direction == 'right' ? "carousel-item-prev" : "carousel-item-next"
         const order_class = direction == 'right' ? "carousel-item-end" : "carousel-item-start"
         ready = false;
-        $(carousel).trigger("slide$bs.carousel");
-        $(items[current]).addClass(direction_class);
-        $(items[current]).addClass(order_class);   //adds left or right to classes
-        console.log(items[current].className);
-        console.log(current)
 
+        //add classes
+        $(items[current]).addClass(direction_class);
+        $(items[current]).addClass(order_class);  
+
+        //remove classes and update and change current index
         setTimeout(() => {
             ready = true
             $(items[current]).removeClass(direction_class);
             $(items[current]).removeClass(order_class);
             $(items[current]).removeClass("active");
-            console.log(items[current].className + " end of hide, removed " +  direction_class);
-            console.log(current)
             current = upcoming
-
         }, 400);
-        
     }
 
+    /*
+    * this function is used to show a carousel item from either the left or right
+    * side of the screen. Takes one parameter, string direction, which is either
+    * 'left' or 'right'.
+    */
     function show(direction){
-
+        //get classes from direction and add class
         const direction_class = direction == 'right' ? 'carousel-item-prev' : 'carousel-item-next'
-    
         $(items[upcoming]).addClass(direction_class);
-        console.log(items[upcoming].className + " midshow");
 
+        //remove classes
         setTimeout(() => {
             $(items[upcoming]).addClass("active");
             $(items[upcoming]).removeClass(direction_class);
             
         }, 200);
-        
     }
 
+    /*
+    * this function moves carousel items on click of previous button
+    */
     $(prev).on('click', function(){
+        //return if not ready
         if (ready!=true){
             return
         }
-
         if (current != 0){ 
             hide('left');
             upcoming = current - 1;
             show('right'); 
         }
-
         else{
+            // only wrap backwards if looping is allowed
             if (loop == true){
                 hide('left');
                 upcoming = items.length - 1;;
@@ -73,32 +79,28 @@ document.querySelectorAll(".carousel").forEach(carousel => {
         
     });
 
+    /*
+    * this function moves carousel items on click of next button
+    */
     $(next).on('click', function(){
+        //return if not ready
         if (ready!=true){
             return
         }
-        
-        //console.log("next" + carousel.id);
         if (current != items.length - 1){
             hide('right');
             upcoming = current + 1;
             show('left');
         }
         else{
+            // only wrap backwards if looping is allowed
             if (loop == true){
-
-                console.log(items[0].className + " befire rm prev");
-                $(items[0]).removeClass("carousel-item-prev");
-                console.log(items[0].className + " after");
-
                 hide('right');
                 upcoming = 0
                 show('left');
             }
         }
     });
-
-
 
 });
 
