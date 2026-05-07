dd004df59655e57dedbdcfb2287c7784d1dd19f5
princie <paperprincie@gmail.com>
2026-05-08 00:06:01 +0800

    Fixed carousel

--- web/static/js/carousel.js
+++ web/static/js/carousel.js
@@ -2,42 +2,52 @@
 document.querySelectorAll(".carousel").forEach(carousel => {
     const items = carousel.querySelectorAll(".carousel-item");
 
-    const prev = carousel.querySelectorAll(".carousel-control-prev");
+    const prev = carousel.querySelectorAll(".carousel-control-prev-icon");
 
-    const next = carousel.querySelectorAll(".carousel-control-next");
+    const next = carousel.querySelectorAll(".carousel-control-next-icon");
 
     let loop = true;
 
     let current = 0;
 
+    let upcoming = 0;
+
     let ready = true;
 
     function hide(direction){
-        
-        const direction_class = direction == 'right' ? 'carousel-item-prev' : 'carousel-item-next'
-        const order_class = direction == 'right' ? 'carousel-item-end' : 'carousel-item-start'
+        const direction_class = direction == 'right' ? "carousel-item-prev" : "carousel-item-next"
+        const order_class = direction == 'right' ? "carousel-item-end" : "carousel-item-start"
         ready = false;
         $(carousel).trigger("slide$bs.carousel");
         $(items[current]).addClass(direction_class);
         $(items[current]).addClass(order_class);   //adds left or right to classes
         console.log(items[current].className);
-        console.log(items[0].className);
-        
+        console.log(current)
+
         setTimeout(() => {
             ready = true
-            $(items[current]).removeClass("active").removeClass(direction_class).removeClass(order_class);
-        }, 200);
+            $(items[current]).removeClass(direction_class);
+            $(items[current]).removeClass(order_class);
+            $(items[current]).removeClass("active");
+            console.log(items[current].className + " end of hide, removed " +  direction_class);
+            console.log(current)
+            current = upcoming
+
+        }, 400);
         
     }
 
     function show(direction){
+
         const direction_class = direction == 'right' ? 'carousel-item-prev' : 'carousel-item-next'
-        $(items[current]).addClass(direction_class);
+    
+        $(items[upcoming]).addClass(direction_class);
+        console.log(items[upcoming].className + " midshow");
+
         setTimeout(() => {
-            console.log(items[current].className);
-            $(items[current]).addClass("active");
-            $(items[current]).removeClass(direction_class);
-            ready = true
+            $(items[upcoming]).addClass("active");
+            $(items[upcoming]).removeClass(direction_class);
+            
         }, 200);
         
     }
@@ -46,16 +56,17 @@ document.querySelectorAll(".carousel").forEach(carousel => {
         if (ready!=true){
             return
         }
-        console.log("previous" + carousel.id);
-        if (current != 0){
+
+        if (current != 0){ 
             hide('left');
-            current -= 1;
-            show('right');
+            upcoming = current - 1;
+            show('right'); 
         }
+
         else{
             if (loop == true){
                 hide('left');
-                current = items.length - 1;;
+                upcoming = items.length - 1;;
                 show('right');
             }
         }
@@ -66,75 +77,28 @@ document.querySelectorAll(".carousel").forEach(carousel => {
         if (ready!=true){
             return
         }
-        console.log("next" + carousel.id);
+        
+        //console.log("next" + carousel.id);
         if (current != items.length - 1){
             hide('right');
-            current += 1;
+            upcoming = current + 1;
             show('left');
         }
         else{
             if (loop == true){
-                
+
+                console.log(items[0].className + " befire rm prev");
+                $(items[0]).removeClass("carousel-item-prev");
+                console.log(items[0].className + " after");
+
                 hide('right');
-                current = 0
+                upcoming = 0
                 show('left');
             }
         }
     });
 
-    items.forEach(item => {
-        console.log($(item).offset());
-    })
-
 
 
 });
 
-
-// internet code i should understand before writing my own jqueury version
-/*
-const carousel = document.getElementById('carouselExampleControls')
-const items = carousel.querySelectorAll('.carousel-item');
-let currentItem = 0;
-let isActive = true;
-
-function setCurrentItem(index) {
-  currentItem = (index + items.length) % items.length;
-}
-
-function hideItem(direction) {
-  isActive = false;
-  items[currentItem].classList.add(direction);
-  items[currentItem].addEventListener('animationend', function() {
-    this.classList.remove('active', direction);
-  });
-}
-
-function showItem(direction) {
-  items[currentItem].classList.add('next', direction);
-  items[currentItem].addEventListener('animationend', function() {
-    this.classList.remove('next', direction);
-    this.classList.add('active');
-    isActive = true;
-  });
-}
-
-document.getElementById('carouselPrev').addEventListener('click', function(e) {
-  e.preventDefault()
-  if (isActive) {
-    hideItem('to-right');
-    setCurrentItem(currentItem - 1);
-    showItem('from-left');
-  }
-});
-
-document.getElementById('carouselNext').addEventListener('click', function(e) {
-  e.preventDefault()
-  if (isActive) {
-    hideItem('to-left');
-    setCurrentItem(currentItem + 1);
-    showItem('from-right');
-  }
-});
-
-*/
