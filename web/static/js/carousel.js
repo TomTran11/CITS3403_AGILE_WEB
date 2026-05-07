9ad5d50f3f835b86aeeff73b480673c5480e7bd2
princie <paperprincie@gmail.com>
2026-05-08 02:14:31 +0800

    added recents carousel

--- web/static/js/carousel.js
+++ web/static/js/carousel.js
@@ -10,7 +10,15 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     let current = 0;
     let upcoming = 0;
     let ready = true;
-    let loop = true;
+    let loop = false;
+    if (Array.from(carousel.classList).includes("loop")){
+        loop = true;
+    }
+    
+    let animation = false; //when false, none of the animation related class changes are applied
+    if (Array.from(carousel.classList).includes("animation")){
+        animation = true;
+    }
     
     /*
     * this function is used to hide a carousel item to either the left or right
@@ -18,23 +26,31 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     * 'left' or 'right'.
     */
     function hide(direction){
-        //get classes from direction
-        const direction_class = direction == 'right' ? "carousel-item-prev" : "carousel-item-next"
-        const order_class = direction == 'right' ? "carousel-item-end" : "carousel-item-start"
         ready = false;
+        if (animation == true){
+            //get classes from direction
+            const direction_class = direction == 'right' ? "carousel-item-prev" : "carousel-item-next";
+            const order_class = direction == 'right' ? "carousel-item-end" : "carousel-item-start";        
 
-        //add classes
-        $(items[current]).addClass(direction_class);
-        $(items[current]).addClass(order_class);  
+            //add classes
+            $(items[current]).addClass(direction_class);
+            $(items[current]).addClass(order_class);  
 
         //remove classes and update and change current index
-        setTimeout(() => {
-            ready = true
-            $(items[current]).removeClass(direction_class);
-            $(items[current]).removeClass(order_class);
+            setTimeout(() => {
+                ready = true;
+                if (animation == true){
+                    $(items[current]).removeClass(direction_class);
+                    $(items[current]).removeClass(order_class);
+                }
+                $(items[current]).removeClass("active");
+                current = upcoming
+            }, 400);
+        }
+        else{
             $(items[current]).removeClass("active");
             current = upcoming
-        }, 400);
+        }
     }
 
     /*
@@ -43,26 +59,33 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     * 'left' or 'right'.
     */
     function show(direction){
-        //get classes from direction and add class
-        const direction_class = direction == 'right' ? 'carousel-item-prev' : 'carousel-item-next'
-        $(items[upcoming]).addClass(direction_class);
+        if (animation == true){
+            //get classes from direction and add class
+            const direction_class = direction == 'right' ? 'carousel-item-prev' : 'carousel-item-next';
+            $(items[upcoming]).addClass(direction_class);
 
-        //remove classes
-        setTimeout(() => {
+            //remove classes
+            setTimeout(() => {  
+                $(items[upcoming]).removeClass(direction_class);
+                $(items[upcoming]).addClass("active");            
+            }, 200);
+        }
+        else{
             $(items[upcoming]).addClass("active");
-            $(items[upcoming]).removeClass(direction_class);
-            
-        }, 200);
+            ready = true;
+        }
 
         //update indicators
         indicator_set = carousel.querySelectorAll(".carousel-indicators")[0];
-        indicators = indicator_set.querySelectorAll("li");
-        for (i=0; i< indicators.length; i++) {
-            if (i == upcoming){
-                $(indicators[i]).addClass("active");
-            }
-            else {
-                $(indicators[i]).removeClass("active");
+        if (indicator_set != undefined){
+            indicators = indicator_set.querySelectorAll("li");
+            for (i=0; i< indicators.length; i++) {
+                if (i == upcoming){
+                    $(indicators[i]).addClass("active");
+                }
+                else {
+                    $(indicators[i]).removeClass("active");
+                }
             }
         }
     }
@@ -76,15 +99,15 @@ document.querySelectorAll(".carousel").forEach(carousel => {
             return
         }
         if (current != 0){ 
-            hide('left');
             upcoming = current - 1;
+            hide('left');
             show('right'); 
         }
         else{
             // only wrap backwards if looping is allowed
             if (loop == true){
+                upcoming = items.length - 1;
                 hide('left');
-                upcoming = items.length - 1;;
                 show('right');
             }
         }
@@ -100,15 +123,15 @@ document.querySelectorAll(".carousel").forEach(carousel => {
             return
         }
         if (current != items.length - 1){
-            hide('right');
             upcoming = current + 1;
+            hide('right');
             show('left');
         }
         else{
             // only wrap backwards if looping is allowed
             if (loop == true){
-                hide('right');
                 upcoming = 0
+                hide('right');
                 show('left');
             }
         }
--- web/static/js/multi_carousel.js
+++ web/static/js/multi_carousel.js
@@ -1,17 +1,24 @@
+document.querySelectorAll(".carousel").forEach(carousel => {
+
+    // CONSTANT DEFINITIONS //
+    const items = carousel.querySelectorAll(".carousel-item");
+    const inner = carousel.querySelectorAll(".carousel-inner");
+    const prev = carousel.querySelectorAll(".carousel-control-prev-icon");
+    const next = carousel.querySelectorAll(".carousel-control-next-icon");
+
+    let carousel_width = $(inner)[0].scrollWidth;
+    let card_width = $(items).width()+40;
+    let scroll_pos = 0;
 
 
-let carousel_width = $('.carousel-inner')[0].scrollWidth;
-let card_width = $('.carousel-item').width()+40;
+    $(next).on('click', function(){
+        scroll_pos += card_width;
+        $(inner).animate({scrollLeft: scroll_pos}, 600);
+    });
 
-let scroll_pos = 0;
+    $(prev).on('click', function(){
+        scroll_pos -= card_width;
+        $(inner).animate({scrollLeft: scroll_pos}, 600);
+    });
 
-
-$('.carousel-control-next').on('click', function(){
-    scroll_pos += card_width;
-    $('.carousel-inner').animate({scrollLeft: scroll_pos}, 600);
-});
-
-$('.carousel-control-prev').on('click', function(){
-    scroll_pos -= card_width;
-    $('.carousel-inner').animate({scrollLeft: scroll_pos}, 600);
 });
