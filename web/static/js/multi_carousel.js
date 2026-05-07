04ba990bbce734ac4c4500bf4d5079a00e5b2d02
princie <paperprincie@gmail.com>
2026-05-08 02:00:56 +0800

    should now work with the new js carousel

--- web/static/js/multi_carousel.js
+++ web/static/js/multi_carousel.js
@@ -1,3 +1,5 @@
+
+
 let carousel_width = $('.carousel-inner')[0].scrollWidth;
 let card_width = $('.carousel-item').width()+40;
 
@@ -5,13 +7,11 @@ let scroll_pos = 0;
 
 
 $('.carousel-control-next').on('click', function(){
-    console.log("scroll");
     scroll_pos += card_width;
     $('.carousel-inner').animate({scrollLeft: scroll_pos}, 600);
 });
 
 $('.carousel-control-prev').on('click', function(){
-    console.log("scroll");
     scroll_pos -= card_width;
     $('.carousel-inner').animate({scrollLeft: scroll_pos}, 600);
 });
